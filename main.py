cbc40e3c964f0d7894769f2eac25fd8c236d87e9
Vladislav Kulik <56491199+VladislavKulik@users.noreply.github.com>
2022-12-26 17:41:24 +0200

    test2
    deleted workers

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot_token = os.environ.get('5957238015:AAHpPAIqxW5PePj0IkMeDx2gfCOky82yhkc')
-workers = int(os.environ.get('4'))
+# workers = int(os.environ.get('4'))
 api = int(os.environ.get('22179434'))
 hash = os.environ.get('c980f4e4e23f03cca2a66ad3d5b3e1c9')
 chnnl = os.environ.get('http://t.me/DankTikTokBot')
 BOT_URL = os.environ.get('DankTikTokBot')
-app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash, workers=workers)
+app = Client("JayBee", bot_token=bot_token, api_id=api, api_hash=hash)
 
 
 
